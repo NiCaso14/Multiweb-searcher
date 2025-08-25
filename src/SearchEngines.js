5e5798a702cc8f6648ad54f2cd4da2b15f5040c5
NiCaso14 <marcoscasodiego@gmail.com>
2025-08-25 10:46:59 +0200

    Create SearchEngines.js

---
+++ src/SearchEngines.js
@@ -0,0 +1,135 @@
+export const SEARCH_ENGINES = [
+  {
+    name: "Google",
+    url: "https://www.google.com/search?q=",
+  },
+  {
+    name: "Microsoft Bing",
+    url: "https://www.bing.com/search?q=",
+  },
+  {
+    name: "Qwant",
+    url: "https://www.qwant.com/?q=",
+  },
+  {
+    name: "Ecosia",
+    url: "https://www.ecosia.org/search?q=",
+  },
+  {
+    name: "DuckDuckGo",
+    url: "https://duckduckgo.com/?q=",
+  },
+  {
+    name: "PrivacyWall",
+    url: "https://privacywall.org/search/?q=",
+  },
+  {
+    name: "Brave",
+    url: "https://search.brave.com/search?q=",
+  },
+  {
+    name: "Yahoo Búsquedas",
+    url: "https://es.search.yahoo.com/search?p=",
+  },
+  {
+    name: "StartPage",
+    url: "https://www.startpage.com/sp/search?query=",
+  },
+  {
+    name: "MetaGer",
+    url: "https://metager.org/meta/meta.ger3?eingabe=",
+  },
+  {
+    name: "YaCy",
+    url: "https://search.yacy.net/search?query=",
+  },
+  {
+    name: "Peekier",
+    url: "https://peekier.com/search?q=",
+  },
+  {
+    name: "Kiddle",
+    url: "https://www.kiddle.co/s.php?q=",
+  },
+  {
+    name: "Yippi",
+    url: "https://www.yippy.com/search?query=",
+  },
+  {
+    name: "WolframAlpha",
+    url: "https://www.wolframalpha.com/input/?i=",
+  },
+  {
+    name: "Bipplex",
+    url: "https://bipplex.com/search?q=",
+  },
+  {
+    name: "Million Short",
+    url: "https://millionshort.com/search?query=",
+  },
+  {
+    name: "Bitclave",
+    url: "https://bitclave.com/search?q=",
+  },
+  {
+    name: "Tineye",
+    url: "https://tineye.com/search/?q=",
+  },
+  {
+    name: "Search Engine Colossus",
+    url: "https://searchenginecolossus.com/search?q=",
+  },
+  {
+    name: "Gibiru",
+    url: "https://gibiru.com/results.html?q=",
+  },
+  {
+    name: "Mojeek",
+    url: "https://www.mojeek.com/search?q=",
+  },
+  {
+    name: "Searx",
+    url: "https://searx.org/search?q=",
+  },
+  {
+    name: "Swisscows",
+    url: "https://swisscows.com/en/web?query=",
+  },
+  {
+    name: "Disconnect Search",
+    url: "https://search.disconnect.me/searchTerms/search?q=",
+  },
+  {
+    name: "Lilo",
+    url: "https://search.lilo.org/results.php?q=",
+  },
+  {
+    name: "Dogpile",
+    url: "https://www.dogpile.com/serp?q=",
+  },
+  // Motores personalizados NiCaso14
+  {
+    name: "NiCaso14 web surfer",
+    url: "https://nicaso14-web-surfer.com/search?q=", // ficticio
+  },
+  {
+    name: "NiCaso14 web surfer private",
+    url: "https://nicaso14-web-surfer.com/private?q=", // ficticio
+  },
+  {
+    name: "NiCaso14 web surfer incognite",
+    url: "https://nicaso14-web-surfer.com/incognite?q=", // ficticio
+  },
+  {
+    name: "NiCaso14 add surfer",
+    url: "https://nicaso14-web-surfer.com/add?q=", // ficticio
+  },
+  {
+    name: "NiCaso14 AI Search",
+    url: "https://nicaso14-web-surfer.com/ai?q=", // ficticio
+  },
+  {
+    name: "NiCaso14 Mini Search",
+    url: "https://nicaso14-web-surfer.com/mini?q=", // ficticio
+  },
+];
